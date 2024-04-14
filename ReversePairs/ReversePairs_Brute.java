f1545b25c6a66c7f2f00c69103666c781c30dc2a
saurabhkk55 <saurabhkumarkardam25@gmail.com>
2024-04-14 19:15:28 +0530

    ReversePairs

---
+++ ReversePairs/ReversePairs_Brute.java
@@ -0,0 +1,24 @@
+class Solution {
+    static long reversePairs (long[] nums) {
+        int n = nums.length;
+        long count = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (nums[i] > 2*nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+class ReversePairs_Brute {
+    public static void main(String[] args) {
+        long[] arr = {40, 25, 19, 12, 9, 6, 2}; // {40, 25, 19, 12, 9, 6, 2}; // {4, 1, 2, 3, 1};
+        long ans = Solution.reversePairs(arr);
+        System.out.println("Reversed pair counts: "+ans);
+    }
+}
+
+// TC: O(n^2)
+// SC: O(1)
---
+++ ReversePairs/ReversePairs_Optimal.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+class Solution {
+    static void merge (long[] arr, int low, int mid, int high) {
+        ArrayList<Long> temp = new ArrayList<>();
+        int left = low, right = mid+1;
+        
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        
+        for (int i=low; i<=high; i++) {
+            arr[i] = temp.get(i-low);
+        }
+    }
+
+    static long countPairs (long[] a, int low, int mid, int high) {
+        int right = mid+1;
+        long count = 0;
+
+        for (int i=low; i<=mid; i++) {
+            while (right <= high && a[i] > 2*a[right]) {
+                right++;
+            }
+            count += (right - (mid+1));
+        }
+        return count;
+    }
+
+    static long ms (long[] a, int low, int high) {
+        long count = 0;
+        if (low >= high) return count;
+
+        int mid = (low+high)/2;
+        count += ms(a, low, mid);
+        count += ms(a, mid+1, high);
+        count += countPairs(a, low, mid, high);
+        merge(a, low, mid, high);
+        return count;
+    }
+
+    static long reversePairs (long[] nums) {
+        int low = 0, high = nums.length-1;
+        return ms(nums, low, high);
+    }
+}
+class ReversePairs_Optimal {
+    public static void main(String[] args) {
+        long[] arr = {40, 25, 19, 12, 9, 6, 2}; // {4, 1, 2, 3, 1};
+        long ans = Solution.reversePairs(arr);
+        System.out.println("Reversed pair counts: "+ans);
+    }
+}
+
+// TC: O(2nlogn)
+// SC: O(n)
