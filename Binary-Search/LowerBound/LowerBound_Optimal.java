d14fa99830f20f38f10f124693719557d7a204f0
saurabhkk55 <saurabhkumarkardam25@gmail.com>
2024-06-20 19:36:58 +0530

    BS docs

--- Binary-Search/LowerBound/LowerBound_Optimal.java
+++ Binary-Search/LowerBound/LowerBound_Optimal.java
@@ -2,22 +2,23 @@
 
 class Solution {
     static int lowerBound (int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int ans = nums.length;
+        int low = 0, high = nums.length - 1;
+        // int ans = nums.length;
 
-        while (left <= right) {
-            int mid = (left+right)/2;
+        while (low <= high) {
+            int mid = (low+high)/2;
 
             if (nums[mid] >= target) {
-                ans = mid;
-                right = mid - 1;
+                // ans = mid;
+                high = mid - 1;
             }
             else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
         
-        return ans;
+        // return ans;
+        return low;
     }
 }
 
--- Binary-Search/UpperBound/UpperBound_Optimal.java
+++ Binary-Search/UpperBound/UpperBound_Optimal.java
@@ -2,29 +2,30 @@
 
 class Solution {
     static int upperBound (int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int ans = nums.length;
+        int low = 0, high = nums.length - 1;
+        // int ans = nums.length;
 
-        while (left <= right) {
-            int mid = (left+right)/2;
+        while (low <= high) {
+            int mid = (low+high)/2;
 
             if (nums[mid] <= target) {
-                left = mid + 1;
+                low = mid + 1;
             }
             else {
-                ans = mid;
-                right = mid - 1;
+                // ans = mid;
+                high = mid - 1;
             }
         }
         
-        return ans;
+        // return ans;
+        return low;
     }
 }
 
 class UpperBound_Optimal {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 8, 8, 10, 10, 11};
-        int target = 5; // 12; // 9; // 1;
+        int target = 1; // 12; // 9; // 1 // 0;
         int ans = Solution.upperBound(arr, target);
         System.out.println("Index at which Element which is just greater than target is "+ans);
     }
